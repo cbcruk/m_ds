b87375d635d42b3eef9adcaebb7fd3ad523dd8a1
cbcruk <cbcruk@gmail.com>
2020-07-17 08:03:44 +0900

    :star: feature: dialog 추가

---
+++ src/Dialog/hooks.ts
@@ -0,0 +1,51 @@
+import { useReducer, Reducer, useCallback } from 'react'
+
+export enum ActionTypes {
+  SHOW_MODAL = 'SHOW_MODAL',
+  HIDE_MODAL = 'HIDE_MODAL'
+}
+
+interface State {
+  isOpen: boolean
+}
+
+interface ShowOrHideAction {
+  type: ActionTypes
+}
+
+type Action = ShowOrHideAction
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case ActionTypes.SHOW_MODAL:
+      return {
+        ...state,
+        isOpen: true
+      }
+    case ActionTypes.HIDE_MODAL:
+      return {
+        ...state,
+        isOpen: false
+      }
+  }
+}
+
+export function useModal() {
+  const [state, dispatch] = useReducer<Reducer<State, Action>>(reducer, {
+    isOpen: false
+  })
+  const handleModal = useCallback(
+    type => {
+      dispatch({
+        type
+      })
+    },
+    [dispatch]
+  )
+
+  return {
+    state,
+    dispatch,
+    handleModal
+  }
+}
---
+++ src/Dialog/style.ts
@@ -0,0 +1,49 @@
+import { css } from 'emotion'
+
+export const wrapper = css({
+  width: 400,
+  minWidth: 400,
+  borderRadius: 4,
+  fontSize: 16
+})
+
+export const header = css({
+  position: 'relative',
+  height: 60,
+  padding: 18,
+  backgroundColor: 'var(--color-brand-parsnip)',
+  textAlign: 'center',
+  color: 'var(--color-brand-peppercorn)'
+})
+
+export const body = css({
+  paddingTop: 24,
+  paddingRight: 18,
+  paddingBottom: 24,
+  paddingLeft: 18
+})
+
+export const footer = css({
+  padding: 18,
+  borderTop: '1px solid #dedddc',
+  fontSize: 14
+})
+
+export const close = css({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  width: 60,
+  height: 60
+})
+
+export const modal = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    transform: 'translate(-50%, -50%)',
+    padding: 0
+  }
+}
