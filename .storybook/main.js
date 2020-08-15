bc9c85c36a044e4d8320c571cd2c994b453b01c6
cbcruk <cbcruk@gmail.com>
2020-08-16 08:51:41 +0900

    :arrow_up: deps: storybook 업그레이드

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,14 @@
-const { resolve } = require('path')
-
 module.exports = {
-  stories: ['../src/**/*.stories.(tsx|mdx)'],
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: [
-    {
-      name: '@storybook/preset-typescript',
-      options: {
-        forkTsCheckerWebpackPluginOptions: {
-          colors: false
-        },
-        include: [resolve('../src')]
-      }
-    },
     {
       name: '@storybook/addon-docs',
       options: {
-        configureJSX: true
-      }
+        configureJSX: true,
+      },
     },
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions',
-    '@storybook/addon-links'
-  ]
+    '@storybook/addon-links',
+  ],
 }
